f94fc979eedb5d10cf757de6ac70a414b9204eb5
Aneesh Shetty <aneeshkshetty@gmail.com>
2023-11-04 14:16:25 -0500

    Updates fslinstaller, and adds custom experiments directory to git

--- tractseg/experiments/custom/my_custom_experiment.py
+++ tractseg/experiments/custom/my_custom_experiment.py
@@ -10,7 +10,7 @@ class Config(TractSegConfig):
 
     DATASET_FOLDER = "/Users/aneeshks/Documents/projects/TractSeg/dataset/camcan"      # name of folder that contains all the preprocessed subjects (each subject has its own folder with the name of the subjectID)
     FEATURES_FILENAME = "mrtrix_peaks"  # filename of nifti file (*.nii.gz) without file ending; mrtrix CSD peaks; shape: [x,y,z,9]; one file for each subject
-    LABELS_FILENAME = 'corrected_bundle_masks'
+    LABELS_FILENAME = 'corrected_tract_masks'
     DATASET = 'camcan'
     CLASSES = 'camcan_all_classes'
     NR_OF_CLASSES = 61
